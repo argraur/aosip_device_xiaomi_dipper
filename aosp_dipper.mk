e17e90349d82dd6badc668e4cde47d400b893360
argraur <argraur@gmail.com>
2018-09-27 16:50:54 +0000

    dipper: use aosp_base_telephony.mk instead of treble common stuff
    Removes O-MR1 support
    
    Signed-off-by: argraur <argraur@gmail.com>

--- aosp_dipper.mk
+++ aosp_dipper.mk
@@ -19,7 +19,7 @@
 
 # Inherit from the common Open Source product treble configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/treble_common_64.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Set dipper info
 PRODUCT_NAME := aosp_dipper
