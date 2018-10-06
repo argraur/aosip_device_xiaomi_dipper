cefd445be09a3b94fd5dfd68890f6b510e1e4807
argraur <argraur@gmail.com>
2018-10-06 19:30:31 +0000

    sepolicy: fix qualcomm time service
    Taken from device/google/wahoo
    
    Signed-off-by: argraur <argraur@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -93,6 +93,9 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += device/xiaomi/dipper/sepolicy/private
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += device/qcom/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += device/qcom/sepolicy/public
 
+# Vendor sepolicy
+BOARD_SEPOLICY_DIRS += device/xiaomi/dipper/sepolicy/vendor
+
 TARGET_EXFAT_DRIVER := exfat
 
 # Filesystem
